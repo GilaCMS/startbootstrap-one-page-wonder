d57288295b78e95c9793f3127bb26bd85f29f272
Jocelyn Reyes <yoce_ab@hotmail.com>
2020-12-01 16:53:35 -0600

    correccion de rutas css y js

--- footer.php
+++ footer.php
@@ -7,13 +7,12 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-<<<<<<< HEAD
+
   <script src="themes/one-page-wonder/vendor/jquery/jquery.min.js"></script>
   <script src="themes/one-page-wonder/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-=======
-  <script src="<?=View::script('themes/one-page-wonder/vendor/jquery/jquery.min.js')?>"></script>
-  <script src="<?=View::script('themes/one-page-wonder/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
->>>>>>> master
+
+  <?=View::script('themes/one-page-wonder/vendor/jquery/jquery.min.js')?>
+  <?=View::script('themes/one-page-wonder/vendor/bootstrap/js/bootstrap.bundle.min.js')?>
 
 </body>
 
--- header.php
+++ header.php
@@ -12,26 +12,25 @@
   <title>One Page Wonder - Start Bootstrap Template</title>
 
   <!-- Bootstrap core CSS -->
-<<<<<<< HEAD
-  <link href="themes/one-page-wonder/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-=======
-  <link href="<?=View::css('themes/one-page-wonder/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
->>>>>>> master
+
+  <?=View::css('themes/one-page-wonder/vendor/bootstrap/css/bootstrap.min.css')?>
+
+  <?=View::css('themes/one-page-wonder/vendor/bootstrap/css/bootstrap.min.css')?>
+
 
   <!-- Custom fonts for this template -->
   <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-<<<<<<< HEAD
-  <link href="themes/one-page-wonder/css/one-page-wonder.min.css" rel="stylesheet">
-  <link href="themes/one-page-wonder/css/one-page-wonder.css" rel="stylesheet">
-  <script type="text/javascript" src="assets/core/gila.min.js"></script>
-=======
-  <link href="<?=View::css('themes/one-page-wonder/css/one-page-wonder.min.css')?>" rel="stylesheet">
-  <link href="<?=View::css('themes/one-page-wonder/css/one-page-wonder.css')?>" rel="stylesheet">
-  <script type="text/javascript" src="<?=View::script('assets/core/gila.min.js')?>"></script>
->>>>>>> master
+
+  <?=View::css('themes/one-page-wonder/css/one-page-wonder.min.css" rel="stylesheet')?>
+  <?=View::css('themes/one-page-wonder/css/one-page-wonder.css')?>
+  <?=View::script('text/javascript" src="assets/core/gila.min.js')?>
+
+  <?=View::css('themes/one-page-wonder/css/one-page-wonder.min.css')?>
+  <?=View::css('themes/one-page-wonder/css/one-page-wonder.css')?>
+  <?=View::script('assets/core/gila.min.js')?>
 
 </head>
 
